050aaeb55aba24962239c02460d0677d07673861
Rupesh <60768695+RupeshAmbade@users.noreply.github.com>
2023-03-01 13:24:46 +0530

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<?php
+
+$url_name = "https://google.com";
+
+  $ch_session = curl_init();
+
+curl_setopt($ch_session, CURLOPT_RETURNTRANSFER, 1);
+
+  curl_setopt($ch_session, CURLOPT_URL, $url_name);
+
+  $result_url = curl_exec($ch_session);
+
+  echo $result_url;
+
+  ?>
+
+</body>
+
+</html>
